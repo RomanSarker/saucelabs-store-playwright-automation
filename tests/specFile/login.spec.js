e72a9afb7fcf5fdf96b91fa3b75a1c74fbf78908
Roman Sarker <romansarker2015@gmail.com>
2026-02-05 14:18:55 +0600

    LoginPageErrMessage under pages done Login spec and Action is running

---
+++ tests/specFile/login.spec.js
@@ -0,0 +1,8 @@
+import {test, expect} from "@playwright/test"
+
+test.describe("Q1-Try login with locked_out_user and verify the error message.",()=>{
+    test("verify the error message with locked_out_user",async({page})=>{
+        
+
+    })
+})
